c261c3ab97cc9415e902bead8f606e84e4225717
Javier Zarate <zarate.javier.jz@gmail.com>
2020-05-26 15:34:06 -0600

    filled controller for teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,13 +1,48 @@
 class TeamsController < ApplicationController
+  before_action :set_team, only: [:edit, :update, :destroy, :show]
+
   def index
+    @teams = Team.all
   end
 
   def show
   end
 
   def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(team_params)
+    if (@team.save)
+      redirect_to teams_path
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def update 
+    if(@team.update(team_params))
+      redirect_to @team
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @team.destroy
+    redirect_to teams_path
+  end
+
+  private
+  def set_team
+    @team = Team.find(params[:id])
+  end
+
+  def team_params
+    params.require(:team).permit(:name)
   end
 end
