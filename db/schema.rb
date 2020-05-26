d5c880d1bf8a0c5fe085aa5ae2ddf1c092a339c9
Javier Zarate <zarate.javier.jz@gmail.com>
2020-05-26 16:59:13 -0600

    added model and controller for player

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_211942) do
+ActiveRecord::Schema.define(version: 2020_05_26_225404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.bigint "team_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["team_id"], name: "index_players_on_team_id"
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
@@ -21,4 +29,5 @@ ActiveRecord::Schema.define(version: 2020_05_26_211942) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "players", "teams"
 end
