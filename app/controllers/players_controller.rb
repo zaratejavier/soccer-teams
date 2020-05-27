5c915c652fb247b3d468482e5d50ebedcebf8266
Javier Zarate <zarate.javier.jz@gmail.com>
2020-05-26 18:49:53 -0600

    view Players button working

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,13 +1,50 @@
 class PlayersController < ApplicationController
-  def index
+  before_action :set_team
+  before_action :set_player, only: [:edit, :update, :show, :destroy]
+
+  def index 
+    @players = @team.players
   end
 
   def show
   end
-
-  def new
+  
+  def edit
   end
 
-  def edit
+  def new
+    @player = @team.players.new
+  end
+
+  def update
+    if @player.update(player_params)
+      redirect_to [@team, @player]
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @player = @team.player.new(player_params)
+    if (@player.save)
+      redirect_to [@team, @player]
+    else
+      render :new
+    end
+  end
+
+
+  
+  private
+  def set_team
+    @team = Team.find(params[:team_id])
+  end
+
+  def set_player
+    @player = Player.find(params[:id])
+  end
+
+  def player_params
+    params.require(:player).permit(:name)
   end
 end
